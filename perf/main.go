0356397c80024b4fa60caeb8cf216fa25bd3ddec
Mrproliu <741550557@qq.com>
2022-01-26 23:47:32 +0800

    update

--- perf/main.go
+++ perf/main.go
@@ -122,20 +122,21 @@ func main() {
 		}
 		fmt.Printf("id: %d, name: %s, stack: %d:%d\n", event.Pid, event.Name, event.KernelStackId, event.UserStackId)
 
-		//fmt.Printf("%d, %d\n", objs.Stacks.KeySize(), objs.Stacks.ValueSize())
-		//var key *uint32
-		//err = objs.Stacks.NextKey(uint32(0), key)
-		//if err != nil {
-		//	log.Printf("err look up : %v", event.UserStackId, err)
-		//	continue
-		//}
-		symbls := make([]uint64, 0)
-		err = objs.Stacks.Lookup(&event.UserStackId, &symbls)
+		fmt.Printf("%d, %d\n", objs.Stacks.KeySize(), objs.Stacks.ValueSize())
+		var key []byte
+		err = objs.Stacks.NextKey(key, &key)
 		if err != nil {
-			log.Printf("err look up stackid: %d, %v", event.UserStackId, err)
+			log.Printf("err look up : %v", event.UserStackId, err)
 			continue
 		}
+		fmt.Printf("key: %v", key)
+		//symbls := make([]uint64, 0)
+		//err = objs.Stacks.Lookup(&event.UserStackId, &symbls)
+		//if err != nil {
+		//	log.Printf("err look up stackid: %d, %v", event.UserStackId, err)
+		//	continue
+		//}
 
-		fmt.Printf("found stacks: %d", len(symbls))
+		//fmt.Printf("found stacks: %d", len(symbls))
 	}
 }

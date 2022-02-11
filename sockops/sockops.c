780a8f2595ae309cfaeeec8c2478422f70f6290f
Mrproliu <741550557@qq.com>
2022-02-11 23:27:55 +0800

    update

--- sockops/sockops.c
+++ sockops/sockops.c
@@ -55,6 +55,93 @@ struct bpf_sock_ops {
 	__u64 bytes_received;
 	__u64 bytes_acked;
 };
+//
+//static __always_inline void sk_extract4_key(const struct bpf_sock_ops *ops,
+//					    struct sock_key *key)
+//{
+//	key->dip4 = ops->remote_ip4;
+//	key->sip4 = ops->local_ip4;
+//	key->family = 1;
+//}
+//
+//static __always_inline void sk_lb4_key(struct lb4_key *lb4,
+//					  const struct sock_key *key)
+//{
+//	/* SK MSG is always egress, so use daddr */
+//	lb4->address = key->dip4;
+//	lb4->dport = (__u16)key->dport;
+//}
+//
+//static __always_inline bool redirect_to_proxy(int verdict)
+//{
+//	return verdict > 0;
+//}
+//
+//static inline void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
+//{
+//	struct lb4_key lb4_key = {};
+//	__u32 dip4, dport, dst_id = 0;
+//	struct endpoint_info *exists;
+//	struct lb4_service *svc;
+//	struct sock_key key = {};
+//	int verdict;
+//
+//	sk_extract4_key(skops, &key);
+//
+//	/* If endpoint a service use L4/L3 stack for now. These can be
+//	 * pulled in as needed.
+//	 */
+//	sk_lb4_key(&lb4_key, &key);
+//	svc = lb4_lookup_service(&lb4_key, true);
+//	if (svc)
+//		return;
+//
+//	/* Policy lookup required to learn proxy port */
+//	if (1) {
+//		struct remote_endpoint_info *info;
+//
+//		info = lookup_ip4_remote_endpoint(key.dip4);
+//		if (info != NULL && info->sec_label)
+//			dst_id = info->sec_label;
+//		else
+//			dst_id = WORLD_ID;
+//	}
+//
+//	verdict = policy_sk_egress(dst_id, key.sip4, (__u16)key.dport);
+//	if (redirect_to_proxy(verdict)) {
+//		__be32 host_ip = IPV4_GATEWAY;
+//
+//		key.dip4 = key.sip4;
+//		key.dport = key.sport;
+//		key.sip4 = host_ip;
+//		key.sport = verdict;
+//
+//		sock_hash_update(skops, &SOCK_OPS_MAP, &key, BPF_NOEXIST);
+//		return;
+//	}
+//
+//	/* Lookup IPv4 address, this will return a match if:
+//	 * - The destination IP address belongs to the local endpoint manage
+//	 *   by Cilium.
+//	 * - The destination IP address is an IP address associated with the
+//	 *   host itself.
+//	 * Then because these are local IPs that have passed LB/Policy/NAT
+//	 * blocks redirect directly to socket.
+//	 */
+//	exists = __lookup_ip4_endpoint(key.dip4);
+//	if (!exists)
+//		return;
+//
+//	dip4 = key.dip4;
+//	dport = key.dport;
+//	key.dip4 = key.sip4;
+//	key.dport = key.sport;
+//	key.sip4 = dip4;
+//	key.sport = dport;
+//
+//	sock_hash_update(skops, &SOCK_OPS_MAP, &key, BPF_NOEXIST);
+//}
+//#endif /* ENABLE_IPV4 */
 
 
 struct key_t {

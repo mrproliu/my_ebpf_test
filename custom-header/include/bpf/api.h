52511b8aa904f79ca244ba7a68cf730051164470
Mrproliu <741550557@qq.com>
2022-02-12 22:09:30 +0800

    update

---
+++ custom-header/include/bpf/api.h
@@ -0,0 +1,8 @@
+#ifndef __BPF_API__
+#define __BPF_API__
+
+#include <linux/bpf.h>
+
+#include "bpf_helpers.h"
+
+#endif
---
+++ custom-header/main.go
@@ -0,0 +1,109 @@
+//go:build linux
+// +build linux
+
+// This program demonstrates attaching an eBPF program to a kernel symbol.
+// The eBPF program will be attached to the start of the sys_execve
+// kernel function and prints out the number of times it has been called
+// every second.
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/perf"
+	"github.com/cilium/ebpf/rlimit"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf kprobe.c -- -Iinclude
+
+type Event struct {
+	Pid    uint32
+	TaskId uint32
+	Name   [128]byte
+}
+
+func main() {
+	// Name of the kernel function to trace.
+	fn := "sys_execve"
+
+	// Allow the current process to lock memory for eBPF resources.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load pre-compiled programs and maps into the kernel.
+	objs := bpfObjects{}
+	spec, err := loadBpf()
+	if err != nil {
+		log.Fatalf("loading objects: %s", err)
+		return
+	}
+	funcName := "kprobe_execve"
+	for i, ins := range spec.Programs[funcName].Instructions {
+		if ins.Reference == "MY_CONST" {
+			ins.Constant = 555
+			fmt.Printf("found the my_const and replaced, index: %d, opCode: %d\n", i, ins.OpCode)
+		}
+	}
+	if err := spec.LoadAndAssign(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %s", err)
+	}
+	defer objs.Close()
+
+	// Open a Kprobe at the entry point of the kernel function and attach the
+	// pre-compiled program. Each time the kernel function enters, the program
+	// will increment the execution counter by 1. The read loop below polls this
+	// map value once per second.
+	kp, err := link.Kprobe(fn, objs.KprobeExecve)
+	if err != nil {
+		log.Fatalf("opening kprobe: %s", err)
+	}
+	defer kp.Close()
+
+	rd, err := perf.NewReader(objs.Counts, os.Getpagesize())
+	if err != nil {
+		log.Fatalf("creating perf event reader: %s", err)
+	}
+
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-stopper
+		log.Println("Received signal, exiting program..")
+
+		rd.Close()
+	}()
+
+	var event Event
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, perf.ErrClosed) {
+				return
+			}
+			log.Printf("reading from perf event reader: %s", err)
+			continue
+		}
+
+		if record.LostSamples != 0 {
+			log.Printf("perf event ring buffer full, dropped %d samples", record.LostSamples)
+			continue
+		}
+
+		// Parse the perf event entry into an Event structure.
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing perf event: %s", err)
+			continue
+		}
+
+		fmt.Printf("pid: %d, taskid: %d, name: %s, stack: %d:%d\n", event.Pid, event.TaskId, event.Name)
+	}
+}

438baa166f6c97e33e867657f25cf92c47a4e5cf
Mrproliu <741550557@qq.com>
2022-02-19 19:45:57 +0800

    update

---
+++ kernel-invoke/main.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+#cgo CFLAGS: -I/usr/src/linux-headers-4.19.0-18-cloud-amd64/include/config
+#include <kallsyms.h>
+*/
+import "C"
+import "fmt"
+
+func main() {
+	buffer := C.CString("")
+	result := C.sprint_symbol(buffer, 111)
+	fmt.Printf("%d", result)
+}

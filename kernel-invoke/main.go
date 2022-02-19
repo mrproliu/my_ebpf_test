127dcf943e02282cc40734452a800f34ec43bfa4
Mrproliu <741550557@qq.com>
2022-02-19 20:26:36 +0800

    update

--- kernel-invoke/main.go
+++ kernel-invoke/main.go
@@ -2,6 +2,10 @@ package main
 
 /*
 extern int sprint_symbol(char *buffer, unsigned long address);
+
+int symbol_query(char *buffer, unsigned long address) {
+	return sprint_symbol(buffer, address)
+}
 */
 import "C"
 import "fmt"
@@ -12,6 +16,6 @@ func main() {
 	//fmt.Printf("%d", result)
 	fmt.Printf("a")
 	buffer := C.CString("")
-	result := C.sprint_symbol(buffer, 111)
+	result := C.symbol_query(buffer, 111)
 	fmt.Printf("%d", result)
 }

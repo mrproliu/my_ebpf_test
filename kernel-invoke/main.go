5b55b3626ea4235888efe7f2b7dfc6c358e1cda6
Mrproliu <741550557@qq.com>
2022-02-20 09:55:58 +0800

    update

--- kernel-invoke/main.go
+++ kernel-invoke/main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-#cgo CFLAGS: -I/usr/src/linux-headers-4.19.0-18-common/include/
+#cgo LDFLAGS: -lelf -lz
 #include <linux/kallsyms.h>
 extern int sprint_symbol(char *buffer, unsigned long address);
 

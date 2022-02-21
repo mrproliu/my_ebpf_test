67a53c57dada41ff9beb9910c300eb2b91da6eac
Mrproliu <741550557@qq.com>
2022-02-21 23:11:59 +0800

    update

--- bio_perf/main.go
+++ bio_perf/main.go
@@ -229,7 +229,7 @@ func testSysSymbol() ([]*kernelSymbol, error) {
 		if strings.HasPrefix(info[0], "ffffffff9435d7d0") {
 			fmt.Printf("Addr: %s, \t, type: %s, symbol: %s, toint: %d\n", info[0], info[1], info[2], uint64(atoi))
 		}
-		//fmt.Printf("%d -> %s\n", atoi, info[2])
+		fmt.Printf("index: %d: %d -> %s\n", count, atoi, info[2])
 		if err != nil {
 			return nil, fmt.Errorf("error read addr: %s, %v", info[0], err)
 		}

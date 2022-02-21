18a3ad5ff0b4b5e9e4934b911f9ad36979b12890
Mrproliu <741550557@qq.com>
2022-02-21 22:02:35 +0800

    update

--- kernel-invoke/main.go
+++ kernel-invoke/main.go
@@ -3,6 +3,12 @@ package main
 /*
 #include <stdio.h>
 
+struct ksym {
+	long addr;
+	char *name;
+};
+#define MAX_SYMS 300000
+static struct ksym syms[MAX_SYMS];
 void demo(){
 	printf("HELLO WORLD! \n");
 }

bbf497238d4dbfe4e013dc9ee29ffd42bf98e299
Mrproliu <741550557@qq.com>
2022-02-21 22:00:14 +0800

    update

--- kernel-invoke/main.go
+++ kernel-invoke/main.go
@@ -1,24 +1,14 @@
 package main
 
 /*
-#cgo LDFLAGS: -lelf -lz
-#cgo CFLAGS: -I/usr/include
-#include <linux/kallsyms.h>
-extern int sprint_symbol(char *buffer, unsigned long address);
+#include <stdio.h>
 
-int symbol_query(char *buffer, unsigned long address) {
-	return sprint_symbol(buffer, address);
+void demo(){
+	printf("HELLO WORLD! \n");
 }
 */
 import "C"
-import "fmt"
 
 func main() {
-	//buffer := C.CString("")
-	//result := C.sprint_symbol(buffer, 111)
-	//fmt.Printf("%d", result)
-	fmt.Printf("a")
-	buffer := C.CString("")
-	result := C.symbol_query(buffer, 111)
-	fmt.Printf("%d", result)
+	C.demo()
 }

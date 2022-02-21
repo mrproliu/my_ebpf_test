3c0cc2497698bdae9454efc767820b4cecebb848
Mrproliu <741550557@qq.com>
2022-02-21 22:04:07 +0800

    update

--- kernel-invoke/main.go
+++ kernel-invoke/main.go
@@ -2,6 +2,13 @@ package main
 
 /*
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <unistd.h>
 
 struct ksym {
 	long addr;
@@ -9,6 +16,39 @@ struct ksym {
 };
 #define MAX_SYMS 300000
 static struct ksym syms[MAX_SYMS];
+static int sym_cnt;
+
+static int ksym_cmp(const void *p1, const void *p2)
+{
+	return ((struct ksym *)p1)->addr - ((struct ksym *)p2)->addr;
+}
+
+int load_kallsyms(void)
+{
+	FILE *f = fopen("/proc/kallsyms", "r");
+	char func[256], buf[256];
+	char symbol;
+	void *addr;
+	int i = 0;
+
+	if (!f)
+		return -ENOENT;
+
+	while (fgets(buf, sizeof(buf), f)) {
+		if (sscanf(buf, "%p %c %s", &addr, &symbol, func) != 3)
+			break;
+		if (!addr)
+			continue;
+		syms[i].addr = (long) addr;
+		syms[i].name = strdup(func);
+		i++;
+	}
+	fclose(f);
+	sym_cnt = i;
+	qsort(syms, sym_cnt, sizeof(struct ksym), ksym_cmp);
+	return 0;
+}
+
 void demo(){
 	printf("HELLO WORLD! \n");
 }
@@ -17,4 +57,5 @@ import "C"
 
 func main() {
 	C.demo()
+	C.load_kallsyms()
 }

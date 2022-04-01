0b4369464ec37cf3d12cab7b9e4eb738812a6ff1
Mrproliu <741550557@qq.com>
2022-04-01 13:21:11 +0800

    update

--- programs/memory.go
+++ programs/memory.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
+//go:noinline
+func test() {
+	math.Sqrt(1000)
+}
 func main() {
 	for true {
-		array := make([]string, 1000)
-		for i := 0; i < 1000; i++ {
-			array[i] = fmt.Sprintf("%d---", i)
-		}
+		//array := make([]string, 1000)
+		//for i := 0; i < 1000; i++ {
+		//	array[i] = fmt.Sprintf("%d---", i)
+		//}
+		test()
 	}
 }

27c4193083c8e2d9194163a824195454e6fa8c20
Mrproliu <741550557@qq.com>
2022-04-22 13:24:43 +0800

    update

--- programs/http.go
+++ programs/http.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"net/http"
 )
 
-func sqrt(x float64) float64 {
-	var r = x
-	var precision = 1e-10
-	var eps = math.Abs(x - r*r)
-	for eps > precision {
-		r = (r + x/r) / 2
-		eps = math.Abs(x - r*r)
-	}
-	return r
-}
-
 func hello(w http.ResponseWriter, req *http.Request) {
 	for {
-		var x = 100000000000000.0
-		sqrt(x)
+		math.Sqrt(100000000000000.0)
 	}
-	w.Write([]byte(fmt.Sprintf("Hello, world!")))
 }
 
 func main() {

2008190f1fd089a77e758693b0f6ae874d573f55
Mrproliu <741550557@qq.com>
2022-04-27 14:27:19 +0800

    update

--- core_read/read.c
+++ core_read/read.c
@@ -10,6 +10,6 @@ SEC("kprobe/sys_execve")
 int do_perf_event(struct pt_regs *ctx) {
     char arg[128];
     bpf_probe_read(&arg, sizeof(arg), (void *)ctx->rdi);
-    bpf_printk("executing , %s\n", arg);
+    bpf_printk("executing , %s\n", *arg);
     return 0;
 }

59f77a767fbaa051ee4145418f37c658f9a0c7c2
Mrproliu <741550557@qq.com>
2022-04-27 23:02:01 +0800

    update1

--- core_read/read.c
+++ core_read/read.c
@@ -4,12 +4,26 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
+struct key_t {
+    char name[100];
+    char comm[100];
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} counts SEC(".maps");
+
 SEC("kprobe/sys_execve")
 int do_perf_event(struct pt_regs *ctx) {
     char filename[100];
     bpf_probe_read(&filename, sizeof(filename),
                        (void *)(long)PT_REGS_PARM1(ctx));
     bpf_trace_printk("executing , %s\n", filename);
+
+    struct key_t key = {.name = filename};
+    bpf_get_current_comm(&key.comm, sizeof(key.comm));
+
+    bpf_perf_event_output(ctx, &counts, BPF_F_CURRENT_CPU, &key, sizeof(key));
     return 0;
 }
 

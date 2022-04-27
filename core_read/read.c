7b776010dac8048d586b4fd113b14c51b1f9b348
Mrproliu <741550557@qq.com>
2022-04-27 14:47:52 +0800

    update

--- core_read/read.c
+++ core_read/read.c
@@ -11,8 +11,6 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 SEC("kprobe/sys_execve")
 int do_perf_event(struct pt_regs *ctx) {
     const char* buf = (const char*)PT_REGS_PARM1(ctx);
-//    char arg[128];
-//    bpf_probe_read(&arg, sizeof(arg), (void *)ctx->rdi);
-    bpf_printk("executing , %s\n", *buf);
+    bpf_printk("executing , %s\n", buf);
     return 0;
 }

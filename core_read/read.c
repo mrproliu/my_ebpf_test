0d60316f74ba260c7271bebda6bb5f7b96a565f2
Mrproliu <741550557@qq.com>
2022-04-28 09:53:33 +0800

    update1

--- core_read/read.c
+++ core_read/read.c
@@ -21,7 +21,7 @@ int do_sys_open(struct pt_regs *ctx) {
 //    bpf_probe_read_user_str(&key.name, sizeof(key.name),
 //                       (void *)(long)ad);
     bpf_probe_read_user_str(&key.name, sizeof(key.name),
-                    (void *)(long)PT_REGS_PARM2(ctx));
+                    (void *)(long)PT_REGS_PARM1(ctx));
     bpf_get_current_comm(&key.comm, sizeof(key.comm));
 
     bpf_perf_event_output(ctx, &counts, BPF_F_CURRENT_CPU, &key, sizeof(key));

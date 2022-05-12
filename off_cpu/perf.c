26434a57e6a6161a00005b08538b16a56263b0ec
Mrproliu <741550557@qq.com>
2022-05-12 15:00:48 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -7,15 +7,15 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 #define MAX_ENTRIES	10000
 
 struct task_struct {
-	__u32 pid;
-    __u32 tgid;
-};
+	__u32 __pid;
+    __u32 __tgid;
+} __attribute__((preserve_access_index));
 
 SEC("kprobe/finish_task_switch")
 int do_finish_task_switch(struct pt_regs *ctx) {
     struct task_struct *p = (void *) PT_REGS_PARM1(ctx);
     __u32 pid = 0;
-    bpf_probe_read(&pid, sizeof(pid), &p->pid);
+    bpf_probe_read(&pid, sizeof(pid), &p->__pid);
     bpf_printk("prev pid: %d\n", pid);
     return 0;
 }

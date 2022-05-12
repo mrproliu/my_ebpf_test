608a4f1c295a68433ba0686b1610cb5f169f5663
Mrproliu <741550557@qq.com>
2022-05-12 14:56:10 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -21,9 +21,10 @@ struct task_struct {
 SEC("kprobe/finish_task_switch")
 int do_finish_task_switch(struct pt_regs *ctx) {
     __u64 id = bpf_get_current_pid_tgid();
-    struct task_struct *p = (void *) bpf_get_current_task();
+    struct task_struct *p = (struct task_struct *) bpf_get_current_task();
     /* record previous thread sleep time */
-    __u32 pid = _(p->pid);
+    __u32 pid = 0;
+    bpf_probe_read(&pid, sizeof(pid), &p->pid);
 //    struct task_struct *p = (void *) PT_REGS_PARM1(ctx);
 //    __u32 pid = 0;
 //    bpf_probe_read_user(&pid, sizeof(pid), &(p->pid));

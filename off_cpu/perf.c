d0014e86821276809e5ae710fc047bc643d83c55
Mrproliu <741550557@qq.com>
2022-05-12 23:20:42 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -10,31 +10,23 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-struct thread_info {
-	long unsigned int flags;
-	long unsigned int syscall_work;
-	__u32 status;
-};
-
-typedef struct {
-	int counter;
-} atomic_t;
-
-struct refcount_struct {
-	atomic_t refs;
-};
-
-typedef struct refcount_struct refcount_t;
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} counts SEC(".maps");
 
 struct task_struct {
 	int pid;
 } __attribute__((preserve_access_index));
 
+struct key_t {
+    __u32 tid;
+};
+
 SEC("kprobe/finish_task_switch")
 int do_finish_task_switch(struct pt_regs *ctx) {
     struct task_struct *p = (struct task_struct *) PT_REGS_PARM1(ctx);
-    __u32 pid = 0;
-    bpf_core_read(&pid, sizeof(pid), &p->pid);
-    bpf_printk("prev status: %v\n", pid);
+    struct key_t key = {};
+    bpf_core_read(&key.tid, sizeof(key.tid), &p->pid);
+    bpf_perf_event_output(ctx, &counts, BPF_F_CURRENT_CPU, &key, sizeof(key));
     return 0;
 }

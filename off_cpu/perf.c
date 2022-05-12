ddf0ab426103563a03d1ef148edeea776ed6f699
Mrproliu <741550557@qq.com>
2022-05-12 21:12:05 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -15,11 +15,18 @@ struct task_struct {
     __u32 tgid;
 };
 
+#define _(P)                                                                   \
+	({                                                                     \
+		typeof(P) val = 0;                                                 \
+		bpf_probe_read_kernel(&val, sizeof(val), &(P));                \
+		val;                                                           \
+	})
+
+
 SEC("kprobe/finish_task_switch")
 int do_finish_task_switch(struct pt_regs *ctx) {
     struct task_struct *p = (void *) PT_REGS_PARM1(ctx);
-    __u32 pid = 0;
-    bpf_probe_read_kernel(&pid, sizeof(pid), &p);
+    __u32 pid = _(p->pid);
     bpf_printk("prev pid: %d\n", pid);
     return 0;
 }

cae2636f94d58c71d55d94e6109d3a40dbab594a
Mrproliu <741550557@qq.com>
2022-05-12 23:30:41 +0800

    update1

--- off_cpu/main.go
+++ off_cpu/main.go
@@ -93,7 +93,7 @@ func main() {
 
 		// Parse the perf event entry into an Event structure.
 		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
-			log.Printf("parsing perf event: %s, original data: %s", err, string(record.RawSample))
+			log.Printf("parsing perf event: %s, original data: %v", err, record.RawSample)
 			continue
 		}
 

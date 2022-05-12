222ecc1bde4e9fb3d49e4b9327cb8a7bc9079ae7
Mrproliu <741550557@qq.com>
2022-05-12 16:01:27 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -1,8 +1,19 @@
 // +build ignore
 
-#include <api.h>
+#include <stddef.h>
+#include <linux/sched.h>
+#include <linux/bpf.h>
+#include <linux/ptrace.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
 char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct task_struct {
+	__u32 pid;
+    __u32 tgid;
+};
 
 SEC("kprobe/finish_task_switch")
 int do_finish_task_switch(struct pt_regs *ctx) {

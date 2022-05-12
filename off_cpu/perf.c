d5a5bc3ff698320cc7c1957b64d2ae41da98eaca
Mrproliu <741550557@qq.com>
2022-05-12 14:49:12 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -7,14 +7,14 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 #define MAX_ENTRIES	10000
 
 struct task_struct {
-	int pid;
-    int tgid;
+	__u32 pid;
+    __u32 tgid;
 };
 
 #define _(P)                                                                   \
 	({                                                                     \
-		typeof(P) val = 0;                                             \
-		bpf_probe_read_user(&val, sizeof(val), &(P));                \
+		typeof(P) val;                                                 \
+		bpf_probe_read_kernel(&val, sizeof(val), &(P));                \
 		val;                                                           \
 	})
 
@@ -22,8 +22,11 @@ SEC("kprobe/finish_task_switch")
 int do_finish_task_switch(struct pt_regs *ctx) {
     __u64 id = bpf_get_current_pid_tgid();
     struct task_struct *p = (void *) PT_REGS_PARM1(ctx);
-    __u32 pid = 0;
-    bpf_probe_read_user(&pid, sizeof(pid), &(p->pid));
+    /* record previous thread sleep time */
+    __u32 pid = _(p->pid);
+//    struct task_struct *p = (void *) PT_REGS_PARM1(ctx);
+//    __u32 pid = 0;
+//    bpf_probe_read_user(&pid, sizeof(pid), &(p->pid));
     bpf_printk("hello: test: %d:%d\n", id, pid);
     return 0;
 }

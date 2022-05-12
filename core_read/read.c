f6e555fc62f4dcac4a34c158bf27bd878fbe6d18
Mrproliu <741550557@qq.com>
2022-05-12 20:15:32 +0800

    update1

--- core_read/read.c
+++ core_read/read.c
@@ -25,13 +25,9 @@ SEC("kprobe/sys_execve")
 int do_sys_execve(struct pt_regs *ctx) {
     struct task_struct *task = (struct task_struct *) bpf_get_current_task();
     struct key_t key = {};
-////    CO-RE way to read
-    long ad = BPF_CORE_READ(ctx, rdi);
-    bpf_probe_read_user_str(&key.name, sizeof(key.name),
-                       (void *)(long)ad);
-    bpf_probe_read(&key.pid, sizeof(key.pid), &task->pid);
-    bpf_probe_read(&key.tgid, sizeof(key.pid), &task->tgid);
-    bpf_probe_read_user_str(&key.name, sizeof(key.name),
+    bpf_probe_read(&key.pid, sizeof(key.pid), &(task->pid));
+    bpf_probe_read(&key.tgid, sizeof(key.pid), &(task->tgid));
+    bpf_probe_read_str(&key.name, sizeof(key.name),
                     (void *)(long)PT_REGS_PARM1(ctx));
     bpf_get_current_comm(&key.comm, sizeof(key.comm));
 

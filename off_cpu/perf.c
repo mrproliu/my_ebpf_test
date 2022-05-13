f2d9cf6155a2ed4c622df278c8edc01bd8551946
Mrproliu <741550557@qq.com>
2022-05-13 08:53:15 +0800

    update1

--- off_cpu/perf.c
+++ off_cpu/perf.c
@@ -1,10 +1,9 @@
 // +build ignore
 
-//#include <stddef.h>
-//#include <linux/sched.h>
-//#include <linux/bpf.h>
-//#include <linux/ptrace.h>
-#include <vmlinux.h>
+#include <stddef.h>
+#include <linux/sched.h>
+#include <linux/bpf.h>
+#include <linux/ptrace.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
@@ -15,6 +14,11 @@ struct {
 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
 } counts SEC(".maps");
 
+struct task_struct {
+	__u32 pid;
+    __u32 tgid;
+};
+
 struct key_t {
     __u32 pid;
     __u32 tgid;
@@ -23,10 +27,9 @@ struct key_t {
 };
 
 SEC("kprobe/finish_task_switch")
-int do_finish_task_switch(struct pt_regs *ctx) {
-    struct task_struct *p = (void *) PT_REGS_PARM1(ctx);
+int do_finish_task_switch(struct pt_regs *ctx, struct task_struct *prev) {
     struct key_t key = {};
-    bpf_probe_read(&key.pid, sizeof(key.pid), &(p->pid));
+    bpf_probe_read(&key.pid, sizeof(key.pid), &(prev->pid));
     bpf_perf_event_output(ctx, &counts, BPF_F_CURRENT_CPU, &key, sizeof(key));
     bpf_printk("prev pid: %d\n", key.pid);
     return 0;

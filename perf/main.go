f83d96a8d5eeb50da9166fc44429c4362bb4ff20
Mrproliu <741550557@qq.com>
2022-05-24 20:43:19 +0800

    update1

--- perf/main.go
+++ perf/main.go
@@ -98,8 +98,8 @@ func main() {
 			Type:        unix.PERF_TYPE_SOFTWARE,
 			Config:      unix.PERF_COUNT_SW_CPU_CLOCK,
 			Sample_type: unix.PERF_COUNT_HW_CPU_CYCLES,
-			Bits:        unix.PerfBitFreq,
-			Sample:      1,
+			Sample:      uint64(time.Second.Nanoseconds()),
+			Wakeup:      1,
 		}
 		fd, err := unix.PerfEventOpen(
 			eventAttr,

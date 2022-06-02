2592f914e709a22583bb07a4c7ba15a41a7171d9
Mrproliu <741550557@qq.com>
2022-06-02 11:48:58 +0800

    update1

--- tcptrace/tcptrace.c
+++ tcptrace/tcptrace.c
@@ -2,48 +2,12 @@
 
 // +build ignore
 
-#include "api.h"
-#include <linux/in6.h>
-#include <linux/net.h>
-#include <linux/socket.h>
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
 char __license[] SEC("license") = "Dual MIT/GPL";
-
-typedef __u64 __addrpair;
-typedef __u32 __portpair;
-
-
-struct sock_common {
-	union {
-		__addrpair skc_addrpair;
-		struct {
-			__be32 skc_daddr;
-			__be32 skc_rcv_saddr;
-		}  __attribute__((preserve_access_index));
-	};
-	union {
-		unsigned int skc_hash;
-		__u16 skc_u16hashes[2];
-	};
-	union {
-		__portpair skc_portpair;
-		struct {
-			__be16 skc_dport;
-			__u16 skc_num;
-		};
-	};
-	short unsigned int skc_family;
-	volatile unsigned char skc_state;
-	unsigned char skc_reuse: 4;
-	unsigned char skc_reuseport: 1;
-	unsigned char skc_ipv6only: 1;
-	unsigned char skc_net_refcnt: 1;
-	int skc_bound_dev_if;
-} __attribute__((preserve_access_index));
-
-struct sock {
-	struct sock_common	__sk_common;
-} __attribute__((preserve_access_index));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
@@ -51,11 +15,6 @@ struct {
 	__uint(value_size, sizeof(struct sock));
     __uint(max_entries, 10000);
 } connect_socks SEC(".maps");
-
-struct sockaddr {
-	__u64	sa_family;	/* address family, AF_xxx	*/
-	char		sa_data[14];	/* 14 bytes of protocol address	*/
-} __attribute__((preserve_access_index));
 
 SEC("kprobe/tcp_connect")
 int bpf_tcp_v4_connect(struct pt_regs *ctx) {

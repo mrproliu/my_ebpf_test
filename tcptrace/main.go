404e01580d355e6df2805007398981058ecb6390
Mrproliu <741550557@qq.com>
2022-06-02 16:47:38 +0800

    a

--- tcptrace/main.go
+++ tcptrace/main.go
@@ -17,42 +17,6 @@ import (
 	"syscall"
 )
 
-// #include <linux/types.h>
-// #include <arpa/inet.h>
-// #include <stdlib.h>
-//
-//char *addr_str(const void *addr, __u32 af) {
-//	size_t size = af == AF_INET ? INET_ADDRSTRLEN : INET6_ADDRSTRLEN;
-//	char *str;
-//
-//	str = malloc(size);
-//	if (!str)
-//		return NULL;
-//
-//	inet_ntop(af, addr, str, size);
-//
-//	return str;
-//}
-//
-//char *get_src_addr(const struct event *ev) {
-//	if (ev->af == AF_INET)
-//		return addr_str(&ev->saddr_v4, ev->af);
-//	else if (ev->af == AF_INET6)
-//		return addr_str(&ev->saddr_v6, ev->af);
-//	else
-//		return NULL;
-//}
-//
-//char *get_dst_addr(const struct event *ev) {
-//	if (ev->af == AF_INET)
-//		return addr_str(&ev->daddr_v4, ev->af);
-//	else if (ev->af == AF_INET6)
-//		return addr_str(&ev->daddr_v6, ev->af);
-//	else
-//		return NULL;
-//}
-import "C"
-
 // $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf tcptrace.c -- -I$HOME/headers/ -D__TARGET_ARCH_x86
 

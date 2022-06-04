68a3b565d61fc946a0b5bd2c5f71a38e16b5b39d
Mrproliu <741550557@qq.com>
2022-06-04 19:25:19 +0800

    update1

--- tcptrace/tcptrace.h
+++ tcptrace/tcptrace.h
@@ -23,9 +23,9 @@ struct sock_opts_event {
     // socket file descriptor
     __u32 sockfd;
     // downstream(only works on server side)
-    __u32 downstream_addr_v4;
-    __u8 downstream_addr_v6[16];
-    __u16 downstream_port;
+//    __u32 downstream_addr_v4;
+//    __u8 downstream_addr_v6[16];
+//    __u16 downstream_port;
     // upstream(works on server and client side)
     __u32 upstream_addr_v4;
     __u8 upstream_addr_v6[16];

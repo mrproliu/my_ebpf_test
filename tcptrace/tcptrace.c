388e5eb4c1d2f889664f7b19eef306bfceafb649
Mrproliu <741550557@qq.com>
2022-06-04 20:16:02 +0800

    update1

--- tcptrace/tcptrace.c
+++ tcptrace/tcptrace.c
@@ -42,6 +42,11 @@ static __inline void submit_new_connection(struct pt_regs* ctx, __u32 from_type,
         bpf_probe_read(&opts_event.downstream_addr_v4, sizeof(opts_event.downstream_addr_v4), &daddr->sin_addr.s_addr);
         bpf_probe_read(&opts_event.downstream_port, sizeof(opts_event.downstream_port), &daddr->sin_port);
         bpf_printk("port: %d, %s\n", opts_event.upstream_port, __BYTE_ORDER__);
+        #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        bpf_printk("little\n");
+        #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        bpf_printk("big\n");
+        #endif
     }
 
     bpf_perf_event_output(ctx, &socket_opts_events_queue, BPF_F_CURRENT_CPU, &opts_event, sizeof(opts_event));

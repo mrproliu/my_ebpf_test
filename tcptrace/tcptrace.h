9514440a31c98ba66bfcd5af133d7edbaa473bfd
Mrproliu <741550557@qq.com>
2022-06-05 15:39:19 +0800

    update1

--- tcptrace/tcptrace.h
+++ tcptrace/tcptrace.h
@@ -81,7 +81,9 @@ static __inline struct sock_data_event_t* create_sock_data() {
     __u32 inx = 0;
     struct sock_data_event_t* data_event = bpf_map_lookup_elem(&sock_data_event_creator_map, &inx);
     if (data_event == NULL) {
-        return NULL;
+        struct sock_data_event_t e = {};
+        bpf_map_update_elem(&sock_data_event_creator_map, &inx, &e, BPF_ANY);
+        return bpf_map_lookup_elem(&sock_data_event_creator_map, &inx);
     }
     return data_event;
 }

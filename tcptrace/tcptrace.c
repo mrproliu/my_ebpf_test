7b35262a645aea956783a9dbee923bfda2793b25
Mrproliu <741550557@qq.com>
2022-06-05 15:41:00 +0800

    update1

--- tcptrace/tcptrace.c
+++ tcptrace/tcptrace.c
@@ -128,17 +128,17 @@ static __inline void process_write_data(struct pt_regs* ctx, __u64 id, struct so
     }
     bpf_printk("sock_data exists\n");
 
-//    const char* buf;
-//    bpf_probe_read(&buf, sizeof(const char*), &args->buf);
-////    struct data_event_t e = {};
-////    bpf_probe_read(&e.data, data_len, buf);
-//    bpf_probe_read(data_event->buf, bytes_count, buf);
-//
-//    if (data_event->buf[0] == 'G' && data_event->buf[1] == 'E' && data_event->buf[2] == 'T') {
-//        bpf_printk("get request \n");
-//    } else {
-//        bpf_printk("unknown\n");
-//    }
+    const char* buf;
+    bpf_probe_read(&buf, sizeof(const char*), &args->buf);
+//    struct data_event_t e = {};
+//    bpf_probe_read(&e.data, data_len, buf);
+    bpf_probe_read(data->buf, bytes_count, buf);
+
+    if (data->buf[0] == 'G' && data->buf[1] == 'E' && data->buf[2] == 'T') {
+        bpf_printk("get request \n");
+    } else {
+        bpf_printk("unknown\n");
+    }
 }
 
 SEC("kretprobe/__sys_sendto")

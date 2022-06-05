1c7284d859dc9438980667d44d1f224a830f7219
Mrproliu <741550557@qq.com>
2022-06-05 21:45:41 +0800

    update1

--- tcptrace/main.go
+++ tcptrace/main.go
@@ -195,7 +195,7 @@ func main() {
 				continue
 			}
 
-			fmt.Printf("DATA send from: %d(%s), protcol: %d, message: %d, socket fd: %d\n", event.Pid, event.Comm, event.ProtocolType, event.MessageType, event.SocketFd)
+			fmt.Printf("DATA send from: %d(%s), protcol: %d, message: %d, socket fd: %d, size: %d\n", event.Pid, event.Comm, event.ProtocolType, event.MessageType, event.SocketFd, event.BufferSize)
 		}
 	}()
 

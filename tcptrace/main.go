2fb97a8431374fa2d3054e492765a67a5309759d
Mrproliu <741550557@qq.com>
2022-06-06 23:41:56 +0800

    update1

--- tcptrace/main.go
+++ tcptrace/main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"ebpf_test/tools/btf"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -114,7 +115,7 @@ func main() {
 
 	// Load pre-compiled programs and maps into the kernel.
 	objs := bpfObjects{}
-	if err := loadBpfObjects(&objs, nil); err != nil {
+	if err := loadBpfObjects(&objs, btf.GetEBPFCollectionOptionsIfNeed()); err != nil {
 		log.Fatalf("loading objects: %s", err)
 	}
 	defer objs.Close()

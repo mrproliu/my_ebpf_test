e3506f582326e2d06112bd726c0c0540e3cbbe22
Mrproliu <741550557@qq.com>
2022-06-07 18:25:21 +0800

    update1

---
+++ tcp_retransmit/main.go
@@ -0,0 +1,113 @@
+//go:build linux
+// +build linux
+
+package main
+
+import (
+	"ebpf_test/tools/btf"
+	"encoding/binary"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"unsafe"
+)
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf tcpretransmit.c -- -I$HOME/headers/ -D__TARGET_ARCH_x86
+
+type TcpRetransmitEvent struct {
+	Pid              uint32
+	Comm             [128]byte
+	Family           uint32
+	UpstreamAddrV4   uint32
+	UpstreamAddrV6   [16]uint8
+	UpstreamPort     uint32
+	DownstreamAddrV4 uint32
+	DownstreamAddrV6 [16]uint8
+	DownstreamPort   uint32
+}
+
+func parsePort(val uint16) uint16 {
+	return binary.BigEndian.Uint16((*(*[2]byte)(unsafe.Pointer(&val)))[:])
+}
+
+func parseAddressV4(val uint32) string {
+	return net.IP((*(*[net.IPv4len]byte)(unsafe.Pointer(&val)))[:]).String()
+}
+
+func parseAddressV6(val [16]uint8) string {
+	return net.IP((*(*[net.IPv6len]byte)(unsafe.Pointer(&val)))[:]).String()
+}
+
+func main() {
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	// Allow the current process to lock memory for eBPF resources.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load pre-compiled programs and maps into the kernel.
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, btf.GetEBPFCollectionOptionsIfNeed()); err != nil {
+		log.Fatalf("loading objects: %s", err)
+	}
+	defer objs.Close()
+
+	kprobe, err := link.Kprobe("tcp_retransmit", objs.TcpRetransmit)
+	if err != nil {
+		log.Fatalf("link tcp drop failure: %v", err)
+	}
+	defer kprobe.Close()
+	log.Printf("start probes success...")
+
+	//eventsFd, err := perf.NewReader(objs.Events, os.Getpagesize())
+	//if err != nil {
+	//	log.Fatalf("creating perf event sock data reader: %s", err)
+	//}
+	//defer eventsFd.Close()
+	//
+	//go func() {
+	//	var event TcpRetransmitEvent
+	//	for {
+	//		record, err := eventsFd.Read()
+	//		if err != nil {
+	//			if errors.Is(err, perf.ErrClosed) {
+	//				return
+	//			}
+	//			log.Printf("reading from perf event reader: %s", err)
+	//			continue
+	//		}
+	//
+	//		if record.LostSamples != 0 {
+	//			log.Printf("perf event ring buffer full, dropped %d samples", record.LostSamples)
+	//			continue
+	//		}
+	//
+	//		// Parse the perf event entry into an Event structure.
+	//		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+	//			log.Printf("parsing perf event: %s", err)
+	//			continue
+	//		}
+	//
+	//		var downstreamAddr, upstreamAddr string
+	//		if syscall.AF_INET == event.Family {
+	//			downstreamAddr = parseAddressV4(event.DownstreamAddrV4)
+	//			upstreamAddr = parseAddressV4(event.UpstreamAddrV4)
+	//		} else {
+	//			downstreamAddr = parseAddressV6(event.DownstreamAddrV6)
+	//			upstreamAddr = parseAddressV6(event.UpstreamAddrV6)
+	//		}
+	//		fmt.Printf("TCP DROP: familu: %d: %s:%d(in %d(%s)) -> %s:%d\n", event.Family, downstreamAddr, parsePort(uint16(event.DownstreamPort)),
+	//			event.Pid, event.Comm, upstreamAddr, parsePort(uint16(event.UpstreamPort)))
+	//	}
+	//}()
+
+	<-stopper
+	log.Println("Received signal, exiting program..")
+}
---
+++ tcp_retransmit/tcpretransmit.c
@@ -0,0 +1,72 @@
+
+// +build ignore
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/sched.h>
+#include <linux/bpf.h>
+#include <linux/ptrace.h>
+#include <linux/socket.h>
+#include <asm/errno.h>
+#include <arpa/inet.h>
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+#include "tcpretransmit.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct tcp_retransmit_event {
+    __u32 pid;
+    char comm[128];
+    __u32 skc_family;
+    __u32 upstream_addr_v4;
+    __u8 upstream_addr_v6[16];
+    __u32 upstream_port;
+    // downstream(only works on server side)
+    __u32 downstream_addr_v4;
+    __u8 downstream_addr_v6[16];
+    __u32 downstream_port;
+};
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(max_entries, 4096);
+} events SEC(".maps");
+
+SEC("kprobe/tcp_retransmit")
+int tcp_retransmit(struct pt_regs *ctx) {
+//    struct sock *s = (void *)PT_REGS_PARM1(ctx);
+//    __u64 id = bpf_get_current_pid_tgid();
+//    __u32 tgid = id >> 32;
+//    struct tcp_drop_event event = {};
+//    event.pid = tgid;
+//    bpf_get_current_comm(&event.comm, sizeof(event.comm));
+//    __u16 skc_family, port;
+//    BPF_CORE_READ_INTO(&skc_family, s, __sk_common.skc_family);
+//    event.skc_family = skc_family;
+//    if (skc_family == AF_INET) {
+//        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_num);
+//        event.downstream_port = port;
+//        BPF_CORE_READ_INTO(&event.downstream_addr_v4, s, __sk_common.skc_rcv_saddr);
+//        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_dport);
+//        event.upstream_port = port;
+//        BPF_CORE_READ_INTO(&event.upstream_addr_v4, s, __sk_common.skc_daddr);
+//        bpf_printk("tcp v4 drop: from: %d:%d\n", event.downstream_addr_v4, event.downstream_port);
+//    } else if (skc_family == AF_INET6) {
+//        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_num);
+//        event.downstream_port = port;
+//        BPF_CORE_READ_INTO(&event.downstream_addr_v6, s, __sk_common.skc_v6_rcv_saddr.in6_u.u6_addr8);
+//        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_dport);
+//        event.upstream_port = port;
+//        BPF_CORE_READ_INTO(&event.upstream_addr_v6, s, __sk_common.skc_v6_daddr.in6_u.u6_addr8);
+//        bpf_printk("tcp v6 drop: from: %s:%d\n", event.downstream_addr_v4, event.downstream_port);
+//    } else {
+//        bpf_printk("now ip drop so ignore: %d\n", skc_family);
+//        return 0;
+//    }
+//    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+    bpf_printk("tcp retransmit call\n");
+    return 0;
+}

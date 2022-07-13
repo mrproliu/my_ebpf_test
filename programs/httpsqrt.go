47280c185b90816d801032d528feaa2ed169b83b
Mrproliu <741550557@qq.com>
2022-07-13 14:51:11 +0800

    update1

---
+++ programs/httpsqrt.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	for {
+		math.Sqrt(1000000000000)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}

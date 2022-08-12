d35e39e595840557874d542b1c6b0c0f9e239748
Mrproliu <741550557@qq.com>
2022-08-12 09:23:32 +0800

    Adding https with java

---
+++ programs/JavaHttps.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JavaHttps {
+    public static void main(String[] args) throws Exception {
+        URL serverUrl = new URL("https://www.baidu.com");
+        HttpURLConnection conn = (HttpURLConnection) serverUrl.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        //必须设置false，否则会自动redirect到重定向后的地址
+        conn.setInstanceFollowRedirects(false);
+        conn.connect();
+        getReturn(conn);
+
+    }
+
+    /*请求url获取返回的内容*/
+    public static String getReturn(HttpURLConnection connection) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        //将返回的输入流转换成字符串
+        try (InputStream inputStream = connection.getInputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);) {
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+            String result = buffer.toString();
+            return result;
+        }
+
+    }
+}

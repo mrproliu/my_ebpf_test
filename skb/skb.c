26694fc193fb436b1c8ea5e8b1881f87cdaedc16
Mrproliu <741550557@qq.com>
2022-08-26 12:31:40 +0800

    Adding https

--- skb/skb.c
+++ skb/skb.c
@@ -32,16 +32,16 @@ int bpf_sockmap(struct __sk_buff *buff)
 //    BPF_CORE_READ_INTO(&s, buff, sk);
 //    bpf_printk("sock addr: %p", s);
 
-    short unsigned int skc_family = 0;
-    BPF_CORE_READ_INTO(&skc_family, buff, family);
-    __u32 local_port = 0;
-    __u32 remote_port = 0;
-    __u16 port = 0;
-
-    BPF_CORE_READ_INTO(&port, buff, local_port);
-    local_port = port;
-    BPF_CORE_READ_INTO(&port, buff, remote_port);
-    remote_port = bpf_ntohs(port);
-	bpf_printk("family: %d, local port: %d, remote_port: %d\n", skc_family, local_port, remote_port);
+//    short unsigned int skc_family = 0;
+//    BPF_CORE_READ_INTO(&skc_family, buff, family);
+//    __u32 local_port = 0;
+//    __u32 remote_port = 0;
+//    __u16 port = 0;
+//
+//    BPF_CORE_READ_INTO(&port, buff, local_port);
+//    local_port = port;
+//    BPF_CORE_READ_INTO(&port, buff, remote_port);
+//    remote_port = bpf_ntohs(port);
+	bpf_printk("family: %d, local port: %d, remote_port: %d\n", buff->family, buff->local_port, buff->remote_port);
 	return 1;
 }

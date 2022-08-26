ecea7142e84122071eaa343de4d5848480d52617
Mrproliu <741550557@qq.com>
2022-08-26 12:27:10 +0800

    Adding https

--- skb/skb.c
+++ skb/skb.c
@@ -28,31 +28,20 @@ struct {
 SEC("cgroup_skb/egress")
 int bpf_sockmap(struct __sk_buff *buff)
 {
-    struct sock* s;
-    BPF_CORE_READ_INTO(&s, buff, sk);
-    bpf_printk("sock addr: %p", s);
+//    struct sock* s;
+//    BPF_CORE_READ_INTO(&s, buff, sk);
+//    bpf_printk("sock addr: %p", s);
 
     short unsigned int skc_family = 0;
-    BPF_CORE_READ_INTO(&skc_family, s, __sk_common.skc_family);
+    BPF_CORE_READ_INTO(&skc_family, buff, family);
     __u32 local_port = 0;
     __u32 remote_port = 0;
     __u16 port = 0;
 
-    if (skc_family == AF_INET) {
-        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_num);
-        local_port = port;
-//        BPF_CORE_READ_INTO(&con.local_addr_v4, s, __sk_common.skc_rcv_saddr);
-        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_dport);
-        remote_port = bpf_ntohs(port);
-//        BPF_CORE_READ_INTO(&con.remote_addr_v4, s, __sk_common.skc_daddr);
-    } else if (skc_family == AF_INET6) {
-        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_num);
-        local_port = port;
-//        BPF_CORE_READ_INTO(&con.local_addr_v6, s, __sk_common.skc_v6_rcv_saddr.in6_u.u6_addr8);
-        BPF_CORE_READ_INTO(&port, s, __sk_common.skc_dport);
-        remote_port = bpf_ntohs(port);
-//        BPF_CORE_READ_INTO(&con.remote_addr_v6, s, __sk_common.skc_v6_daddr.in6_u.u6_addr8);
-   }
+    BPF_CORE_READ_INTO(&port, buff, local_port);
+    local_port = port;
+    BPF_CORE_READ_INTO(&port, buff, remote_port);
+    remote_port = bpf_ntohs(port);
 	bpf_printk("family: %d, local port: %d, remote_port: %d\n", skc_family, local_port, remote_port);
 	return 1;
 }

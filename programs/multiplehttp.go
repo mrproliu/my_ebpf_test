cd5274748a7955578447f05408ebdab4b2fbe215
Mrproliu <741550557@qq.com>
2022-10-10 12:55:32 +0800

    Adding https

---
+++ programs/multiplehttp.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 3; i++ {
+		go func() {
+			for true {
+				t, _ := http.Get("http://www.baidu.com")
+				t.Body.Close()
+				time.Sleep(1 * time.Second)
+			}
+		}()
+	}
+}

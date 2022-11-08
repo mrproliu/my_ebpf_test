150b39ca8f244f3ac74befe8ca68415c3cea8b29
Mrproliu <741550557@qq.com>
2022-11-08 19:51:45 +0800

    Adding https

---
+++ time/main.go
@@ -0,0 +1,89 @@
+//go:build linux
+// +build linux
+
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/perf"
+	"github.com/cilium/ebpf/rlimit"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+)
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf time.c -- -I../headers
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal("please input the pid need to be monitor")
+		return
+	}
+	pid, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("could not reconized the pid: %s", os.Args[1])
+		return
+	}
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	// Allow the current process to lock memory for eBPF resources.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load pre-compiled programs and maps into the kernel.
+	objects := bpfObjects{}
+	if err := loadBpfObjects(objects, nil); err != nil {
+		log.Fatalf("loading objects: %s", err)
+	}
+	defer objects.Close()
+
+	executable, err := link.OpenExecutable(fmt.Sprintf("/proc/%d/exe", pid))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uretprobe, err := executable.Uretprobe("time.Now", objects.DoPerfEvent, nil)
+
+	rd, err := perf.NewReader(objects.Counts, os.Getpagesize())
+	if err != nil {
+		log.Fatalf("creating perf event reader: %s", err)
+	}
+	defer rd.Close()
+	defer uretprobe.Close()
+
+	log.Printf("Listening for events..")
+
+	var event uint64
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, perf.ErrClosed) {
+				return
+			}
+			log.Printf("reading from perf event reader: %s", err)
+			continue
+		}
+
+		if record.LostSamples != 0 {
+			log.Printf("perf event ring buffer full, dropped %d samples", record.LostSamples)
+			continue
+		}
+
+		// Parse the perf event entry into an Event structure.
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing perf event: %s", err)
+			continue
+		}
+
+		fmt.Printf("---------------%d\n", event)
+	}
+}
---
+++ time/time.c
@@ -0,0 +1,26 @@
+// +build ignore
+
+#include "common.h"
+#include "bpf_helpers.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+#define MAX_ENTRIES	10000
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} counts SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_STACK_TRACE);
+    __uint(key_size, sizeof(u32));
+    __uint(value_size, 100 * sizeof(u64));
+    __uint(max_entries, 10000);
+} stacks SEC(".maps");
+
+SEC("perf_event")
+int do_perf_event(struct pt_regs *ctx) {
+    __u64 time = bpf_ktime_get_ns();
+    bpf_perf_event_output(ctx, &counts, BPF_F_CURRENT_CPU, &time, sizeof(time));
+    return 0;
+}

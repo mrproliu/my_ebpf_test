6db329ac43670384debe58d869946c6e8103d636
Mrproliu <741550557@qq.com>
2022-11-23 20:02:53 +0800

    Adding https

--- programs/tracing/http.go
+++ programs/tracing/http.go
@@ -18,7 +18,9 @@
 package main
 
 import (
+	"context"
 	"github.com/SkyAPM/go2sky"
+	"github.com/SkyAPM/go2sky/reporter"
 	"github.com/openzipkin/zipkin-go"
 	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
 	zipkin_reporter "github.com/openzipkin/zipkin-go/reporter/http"
@@ -32,68 +34,68 @@ var skyWalkingTracer *go2sky.Tracer
 var zipkinTracer *zipkin.Tracer
 
 func main() {
-	//// init skywalking tracer
-	//r, err := reporter.NewGRPCReporter("localhost:11800")
-	//if err != nil {
-	//	log.Fatalf("new reporter error %v \n", err)
-	//}
-	//defer r.Close()
-	//skyWalkingTracer, err = go2sky.NewTracer("example", go2sky.WithReporter(r))
-	//if err != nil {
+	// init skywalking tracer
+	r, err := reporter.NewGRPCReporter("localhost:11800")
+	if err != nil {
+		log.Fatalf("new reporter error %v \n", err)
+	}
+	defer r.Close()
+	skyWalkingTracer, err = go2sky.NewTracer("example", go2sky.WithReporter(r))
+	if err != nil {
 	//	log.Fatalf("init skyWalkingTracer failure: %v", err)
 	//}
-
-	// init zipkin tracer
-	zipkinReporter := zipkin_reporter.NewReporter("http://localhost:9411/api/v2/spans")
-	// create our local service endpoint
-	endpoint, err := zipkin.NewEndpoint("zipkin-service", "localhost:0")
-	if err != nil {
-		log.Fatalf("unable to create local endpoint: %+v\n", err)
-	}
-	// initialize our tracer
-	zipkinTracer, err = zipkin.NewTracer(zipkinReporter, zipkin.WithLocalEndpoint(endpoint))
-	if err != nil {
-		log.Fatalf("unable to create tracer: %+v\n", err)
-	}
+	//
+	//// init zipkin tracer
+	//zipkinReporter := zipkin_reporter.NewReporter("http://localhost:9411/api/v2/spans")
+	//// create our local service endpoint
+	//endpoint, err := zipkin.NewEndpoint("zipkin-service", "localhost:0")
+	//if err != nil {
+	//	log.Fatalf("unable to create local endpoint: %+v\n", err)
+	//}
+	//// initialize our tracer
+	//zipkinTracer, err = zipkin.NewTracer(zipkinReporter, zipkin.WithLocalEndpoint(endpoint))
+	//if err != nil {
+	//	log.Fatalf("unable to create tracer: %+v\n", err)
+	//}
 
 	// zipkin
-	for true {
-		//addr := "http://www.google.com"
-		addr := "http://google.com/3333.html"
-		request, err := http.NewRequest("GET", addr, nil)
-		client, err := zipkinhttp.NewClient(zipkinTracer, zipkinhttp.ClientTrace(true))
-		if err != nil {
-			log.Fatalf("unable to create client: %+v\n", err)
-		}
-		get, err := client.Do(request)
-		if err != nil {
-			log.Printf("send request error: %v", err)
-		}
-		_, err = ioutil.ReadAll(get.Body)
-		_ = get.Body.Close()
-		if err != nil {
-			log.Printf("get response body error: %v", err)
-		}
-
-		time.Sleep(time.Second)
-	}
-
-	//// skywalking
 	//for true {
-	//	addr := "http://www.baidu.com"
+	//	//addr := "http://www.google.com"
+	//	addr := "http://google.com/3333.html"
 	//	request, err := http.NewRequest("GET", addr, nil)
-	//	exitSpan, err := skyWalkingTracer.CreateExitSpan(context.Background(), "/curl", addr, func(headerKey, headerValue string) error {
-	//		request.Header.Set(headerKey, headerValue)
-	//		return nil
-	//	})
-	//	get, err := http.DefaultClient.Do(request)
+	//	client, err := zipkinhttp.NewClient(zipkinTracer, zipkinhttp.ClientTrace(true))
+	//	if err != nil {
+	//		log.Fatalf("unable to create client: %+v\n", err)
+	//	}
+	//	get, err := client.Do(request)
+	//	if err != nil {
+	//		log.Printf("send request error: %v", err)
+	//	}
 	//	_, err = ioutil.ReadAll(get.Body)
 	//	_ = get.Body.Close()
-	//	exitSpan.End()
 	//	if err != nil {
 	//		log.Printf("get response body error: %v", err)
 	//	}
 	//
 	//	time.Sleep(time.Second)
 	//}
+
+	// skywalking
+	for true {
+		addr := "http://www.google.com"
+		request, err := http.NewRequest("GET", addr, nil)
+		exitSpan, err := skyWalkingTracer.CreateExitSpan(context.Background(), "/curl", addr, func(headerKey, headerValue string) error {
+			request.Header.Set(headerKey, headerValue)
+			return nil
+		})
+		get, err := http.DefaultClient.Do(request)
+		_, err = ioutil.ReadAll(get.Body)
+		_ = get.Body.Close()
+		exitSpan.End()
+		if err != nil {
+			log.Printf("get response body error: %v", err)
+		}
+
+		time.Sleep(time.Second)
+	}
 }

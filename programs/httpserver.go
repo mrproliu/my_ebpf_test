418878e196a8ed9fce7659c8fdaba504037d9e7b
Mrproliu <741550557@qq.com>
2023-02-20 17:32:46 +0800

    http with error

--- programs/httpserver.go
+++ programs/httpserver.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 )
 
 func main() {
 	http.HandleFunc("/provider", func(writer http.ResponseWriter, request *http.Request) {
+		defer request.Body.Close()
+		if rand.Float32() > 0.5 {
+			writer.WriteHeader(500)
+			return
+		}
 		time.Sleep(time.Second * 2)
 		writer.Write([]byte("ok"))
-		_ = request.Body.Close()
 	})
 
 	err := http.ListenAndServe(":9999", nil)

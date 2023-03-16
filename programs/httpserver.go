1b2872d8a492e65d09b2f48b6b601fba6592691e
Mrproliu <741550557@qq.com>
2023-03-16 09:05:39 +0800

    update

--- programs/httpserver.go
+++ programs/httpserver.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"net/http"
 	"time"
 )
@@ -10,10 +9,10 @@ import (
 func main() {
 	http.HandleFunc("/provider", func(writer http.ResponseWriter, request *http.Request) {
 		defer request.Body.Close()
-		if rand.Float32() > 0.5 {
-			writer.WriteHeader(500)
-			return
-		}
+		//if rand.Float32() > 0.5 {
+		//	writer.WriteHeader(500)
+		//	return
+		//}
 		time.Sleep(time.Second * 2)
 		writer.Write([]byte("ok"))
 	})
